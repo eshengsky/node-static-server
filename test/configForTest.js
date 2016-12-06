a952f02836a399b27fae601246a8d643e6ef43f9
Sky <eshengsky@163.com>
2016-12-07 00:41:07 +0800

    完善测试脚本

--- test/configForTest.js
+++ test/configForTest.js
@@ -2,18 +2,33 @@
  * Created by Sky on 2016/12/6.
  */
 exports.config = {
-    // 绑定端口
+    /**
+     * 绑定主机
+     */
+    host: '127.0.0.1',
+
+    /**
+     * 绑定端口
+     */
     port: 12321,
 
-    // 欢迎页面
+    /**
+     * 欢迎页面显示
+     */
     welcome: 'Test Welcome Page!',
 
-    // 资源文件顶层目录
+    /**
+     * 资源文件最外层目录
+     */
     assets: './assetsForTest/',
 
-    // 缓存过期时间（单位秒）
+    /**
+     * 缓存过期时间（单位秒）
+     */
     maxAge: 60 * 60 * 24 * 30,
 
-    // gzip压缩文件的类型
+    /**
+     * gzip压缩文件的类型
+     */
     gzipTypes: /js|css|html?/i
 };
--- test/test.js
+++ test/test.js
@@ -5,15 +5,41 @@
 const assert = require('assert'),
     {exec} = require('child_process'),
     request = require('request'),
-    {config} = require('./configForTest');
+    {config} = require('./configForTest'),
+    website = `http://${config.host}:${config.port}`;
 
 describe('node-static-server tests', () => {
-    it('创建静态资源服务器', (done) => {
+    it('创建静态资源服务器', done => {
         var child = exec('node ./server.js ./test/configForTest.js');
         child.stdout.on('data', success);
         function success(data) {
-            assert.equal(data, `Static server is running at http://127.0.0.1:${config.port}\n`);
+            assert.equal(data, `Static server is running at ${website}\n`);
             done();
         }
+    });
+
+    it('测试欢迎页面', done => {
+        request(website, (err, res, body) => {
+            assert.ifError(err);
+            assert.equal(body, config.welcome);
+            done();
+        })
+    });
+
+    it('只允许GET请求方式', done => {
+        request.post(website, (err, res, body) => {
+            assert.ifError(err);
+            assert.equal(res.statusCode, 400);
+            assert.equal(body, '400 Bad Request');
+            done();
+        })
+    });
+
+    it('测试欢迎页面', done => {
+        request(website, (err, res, body) => {
+            assert.ifError(err);
+            assert.equal(body, config.welcome);
+            done();
+        })
     });
 });
